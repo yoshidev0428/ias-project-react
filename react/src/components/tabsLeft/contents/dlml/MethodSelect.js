ea76694bc3fce7334c6c9c9487dddde3360fa24d
biginfo2012 <83626701+biginfo2012@users.noreply.github.com>
2022-11-26 17:53:26 +0800

    cellpose UI

--- react/src/components/tabsLeft/contents/dlml/MethodSelect.js
+++ react/src/components/tabsLeft/contents/dlml/MethodSelect.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import SmallCard from "../../../custom/SmallCard"
-import CustomButton from "../../../custom/CustomButton"
-import { 
-    mdiPlusBox,
-    mdiPlayBox
-} from '@mdi/js';
+import Button from '@mui/material/Button'
+import {useFlagsStore} from "../../../state"
+import BasicDialog from "./dialog/BasicDialog"
+import CustomDialog from "./dialog/CustomDialog"
+import CellposeDialog from "./dialog/CellposeDialog"
 export default function MethodSelect () {
-    const onNew = () => {
-        console.log("New")
+    const DialogCellposeFlag = useFlagsStore(store => store.DialogCellposeFlag)
+    const DialogBasicFlag = useFlagsStore(store => store.DialogBasicFlag)
+    const DialogCustomFlag = useFlagsStore(store => store.DialogCustomFlag)
+    const showBasicDialog = () => {
+        useFlagsStore.setState({ DialogBasicFlag: true })
+    }
+    const showCustomDialog = () => {
+        useFlagsStore.setState({ DialogCustomFlag: true })
     }
     const onCall = () => {
         console.log("Call")
@@ -16,11 +22,13 @@ export default function MethodSelect () {
     return (
         <div className=''>
             <SmallCard title="Method Select">
-                {/* <CustomButton icon={mdiPlusBox} label="New" onClick={() => onNew()} />
-                <CustomButton icon={mdiPlayBox} label="Call" onClick={() => onCall()} /> */}
-                <CustomButton icon={mdiPlusBox} onClick={() => onNew()} />
-                <CustomButton icon={mdiPlayBox} onClick={() => onCall()} />
+                <Button style={{color: "rgb(15, 150, 136)"}} onClick={showBasicDialog}>Basic</Button>
+                <Button style={{color: "rgb(15, 150, 136)"}} onClick={onCall}>Advance</Button>
+                <Button style={{color: "rgb(15, 150, 136)"}} onClick={showCustomDialog}>Custom</Button>
             </SmallCard>
+            {DialogBasicFlag && <BasicDialog />}
+            {DialogCustomFlag && <CustomDialog />}
+            {DialogCellposeFlag && <CellposeDialog />}
         </div>
     )
 }
