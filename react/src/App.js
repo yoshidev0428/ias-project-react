5e9ea3f6b9a83e8b31aa0eda07e4d7ddf708bee4
Oleksandr Afanasiev <alexandar1992011@outlook.com>
2023-03-02 07:17:56 +0900

    fix: set brower env to eslintrc

--- react/src/App.js
+++ react/src/App.js
@@ -1,33 +1,14 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import './App.scss';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Auth from './pages/auth';
 import MainFrame from './pages/MainFrame';
 
-const mapStateToProps = state => ({
-    isShowAuthPage: state.auth.authPage !== null,
-    isLoggedIn: !state.auth.isLoggedIn
-})
+const App = () => {
+  const authValid = useSelector((state) => !state.auth.authPage);
 
-const App = (props) => {
+  return authValid ? <Auth /> : <MainFrame />;
+};
 
-    useEffect(() => {
-        const script = document.createElement('script');
-        // script.src = "https://cdn.jsdelivr.net/npm/plotty";
-        script.async = true;
-        document.body.appendChild(script);
-        return () => {
-            document.body.removeChild(script);
-        }
-    }, []);
-
-    return (
-      <>
-        {props.isShowAuthPage ? <Auth /> : <MainFrame />}
-      </>
-    );
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
