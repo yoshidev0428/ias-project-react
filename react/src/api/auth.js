fd84ecf3192859964e64bb0a8559101f6fc042a9
Oleksandr Afanasiev <alexandar1992011@outlook.com>
2023-03-02 07:21:00 +0900

    fix: set node env true in eslintrc

--- react/src/api/auth.js
+++ react/src/api/auth.js
@@ -1,41 +1,42 @@
-import {api} from "./base";
+import { api } from './base';
 import store from '../reducers';
-import axios from "axios";
+import axios from 'axios';
 
-export const login = params => {
-    const formData = new FormData();
-    formData.append("username", params.email); // email is username
-    formData.append("password", params.password);
-    formData.append("otp", params.otp);
-    return api.post("auth/login", formData);
+export const login = (params) => {
+  const formData = new FormData();
+  formData.append('username', params.email); // email is username
+  formData.append('password', params.password);
+  formData.append('otp', params.otp);
+  return api.post('auth/login', formData);
 };
 
-// export const logout = params => {
-//   return api.post("logout", params);
-// };
-
-export const register_user = params => {
-    return api.post("auth/register", params);
+export const register_user = (params) => {
+  return api.post('auth/register', params);
 };
-
 
 // USER SUPPORT CHAT
 export const submit_message = async (msg) => {
-    var state = store.getState();
-    let user = state.auth.user;
-    if (user === null || user.email === undefined || user.email === null) {
-        return {data: "", status: ""};
-    } else {
-        const data = {
-            "text": `Name: ${user.fullName} \nEmail: ${user.email} \nContent: ${msg}`,
-        }
-        let res = await axios.post(process.env.REACT_APP_CHAT_SLACK_WEBHOOK, JSON.stringify(data), {
-            withCredentials: false,
-            transformRequest: [(data, headers) => {
-                delete headers.post["Content-Type"]
-                return data
-            }]
-        });
-        return res;
-    }
-}
+  var state = store.getState();
+  let user = state.auth.user;
+  if (user === null || user.email === undefined || user.email === null) {
+    return { data: '', status: '' };
+  } else {
+    const data = {
+      text: `Name: ${user.fullName} \nEmail: ${user.email} \nContent: ${msg}`,
+    };
+    let res = await axios.post(
+      process.env.REACT_APP_CHAT_SLACK_WEBHOOK,
+      JSON.stringify(data),
+      {
+        withCredentials: false,
+        transformRequest: [
+          (data, headers) => {
+            delete headers.post['Content-Type'];
+            return data;
+          },
+        ],
+      },
+    );
+    return res;
+  }
+};
