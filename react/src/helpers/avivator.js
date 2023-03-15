57e9b124c09cd589a0e15e480d263d4693fd33c1
james <klegendary7811@gmail.com>
2023-03-15 07:46:19 +0100

    fix image loading issue

--- react/src/helpers/avivator.js
+++ react/src/helpers/avivator.js
@@ -152,9 +152,14 @@ export async function createLoader(
       }
 
       const url = urlOrFile;
-      const res = await fetch(url.replace(/ome\.tif(f?)/gi, 'offsets.json'));
-      const isOffsetsNot200 = res.status !== 200;
-      const offsets = !isOffsetsNot200 ? await res.json() : undefined;
+      let offsets = undefined;
+      let isOffsetsNot200 = false;
+      try {
+        const res = await fetch(url.replace(/ome\.tif(f?)/gi, 'offsets.json'));
+        isOffsetsNot200 = res.status !== 200;
+        offsets = !isOffsetsNot200 ? await res.json() : undefined;
+      } catch (e1) {}
+
       // TODO(2021-05-06): temporarily disable `pool` until inline worker module is fixed.
       const source = await loadOmeTiff(urlOrFile, {
         offsets,
