9c90279c3353fcad042a9a8095eca8351c403358
gtgjpj <115956788+gtgjpj@users.noreply.github.com>
2023-03-31 05:58:10 +0900

    hotfix: wrong scope of 'git revert'

--- react/src/helpers/file.js
+++ react/src/helpers/file.js
@@ -85,12 +85,11 @@ export const getImageUrl = (
     url = `${userId}/${path}`;
   }
 
-  // 20230331
-  // if (resumeDowloading) {
-  //   url = `image/download/?path=${url}`;
-  // } else {
-  url = `static/${url}`;
-  // }
+  if (resumeDowloading) {
+    url = `image/download/?path=${url}`;
+  } else {
+    url = `static/${url}`;
+  }
 
   return `${process.env.REACT_APP_BASE_API_URL}/${url}`.replaceAll(/\/+/g, '/');
 };
