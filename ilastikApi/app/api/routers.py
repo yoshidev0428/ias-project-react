ce0724c4b456551d9384bf2225d6c6a2f30b8616
james <klegendary7811@gmail.com>
2023-04-03 23:35:15 +0200

    create project test

--- ilastikApi/app/api/routers.py
+++ ilastikApi/app/api/routers.py
@@ -67,86 +67,86 @@ async def processImage(request: Request):
     newProjectFile.close()
 
 
-    shell = HeadlessShell()
-    shell.openProjectFile(project_file_path)
-    workflow = shell.workflow
-
-    # Add a file
-    from ilastik.applets.dataSelection.opDataSelection import FilesystemDatasetInfo
-
-    info = FilesystemDatasetInfo(filePath=imagePath)
-    opDataSelection = workflow.dataSelectionApplet.topLevelOperator
-    opDataSelection.DatasetGroup.resize(1)
-    opDataSelection.DatasetGroup[0][0].setValue(info)
-
-    # Set some features
-    ScalesList = [0.3, 0.7, 1, 1.6, 3.5, 5.0, 10.0]
-    FeatureIds = [
-        "GaussianSmoothing",
-        "LaplacianOfGaussian",
-        "StructureTensorEigenvalues",
-        "HessianOfGaussianEigenvalues",
-        "GaussianGradientMagnitude",
-        "DifferenceOfGaussians",
-    ]
-
-    opFeatures = workflow.featureSelectionApplet.topLevelOperator
-    opFeatures.Scales.setValue(ScalesList)
-    opFeatures.FeatureIds.setValue(FeatureIds)
-
-    #                    sigma:   0.3    0.7    1.0    1.6    3.5    5.0   10.0
-    selections = numpy.array(
-        [
-            [True, False, False, False, False, False, False],
-            [True, False, False, False, False, False, False],
-            [True, False, False, False, False, False, False],
-            [False, False, False, False, False, False, False],
-            [False, False, False, False, False, False, False],
-            [False, False, False, False, False, False, False],
-        ]
-    )
-    opFeatures.SelectionMatrix.setValue(selections)
-
-    # Add some labels directly to the operator
-    opPixelClass = workflow.pcApplet.topLevelOperator
-
-    opPixelClass.LabelNames.setValue(["Label 1", "Label 2"])
-
-    slicing1 = sl[0:1, 0:10, 0:10, 0:1, 0:1]
-    labels1 = 1 * numpy.ones(slicing2shape(slicing1), dtype=numpy.uint8)
-    opPixelClass.LabelInputs[0][slicing1] = labels1
-
-    slicing2 = sl[0:1, 0:10, 10:20, 0:1, 0:1]
-    labels2 = 2 * numpy.ones(slicing2shape(slicing2), dtype=numpy.uint8)
-    opPixelClass.LabelInputs[0][slicing2] = labels2
-
-    # Train the classifier
-    opPixelClass.FreezePredictions.setValue(False)
-    _ = opPixelClass.Classifier.value
-
-    # Save and close
-    shell.projectManager.saveProject()
-    shell.closeCurrentProject()
-
-    args = "--project=" + project_file_path
-    args += " --headless"
-
-    # Batch export options
-    args += " --output_format=tiff"
-    args += " --output_filename_format={dataset_dir}/{nickname}_prediction.tiff"
-    args += " --output_internal_path=volume/pred_volume"
-    args += " --raw_data"
-    # test that relative path works correctly: should be relative to cwd, not project file.
-    args += " " + imagePath
-
-    old_sys_argv = list(sys.argv)
-    sys.argv = ["ilastik.py"]  # Clear the existing commandline args so it looks like we're starting fresh.
-    sys.argv += args.split()
-
-    # Start up the ilastik.py entry script as if we had launched it from the command line
-    try:
-        ilastik_startup.main()
-    finally:
-        sys.argv = old_sys_argv
+    # shell = HeadlessShell()
+    # shell.openProjectFile(project_file_path)
+    # workflow = shell.workflow
+    #
+    # # Add a file
+    # from ilastik.applets.dataSelection.opDataSelection import FilesystemDatasetInfo
+    #
+    # info = FilesystemDatasetInfo(filePath=imagePath)
+    # opDataSelection = workflow.dataSelectionApplet.topLevelOperator
+    # opDataSelection.DatasetGroup.resize(1)
+    # opDataSelection.DatasetGroup[0][0].setValue(info)
+    #
+    # # Set some features
+    # ScalesList = [0.3, 0.7, 1, 1.6, 3.5, 5.0, 10.0]
+    # FeatureIds = [
+    #     "GaussianSmoothing",
+    #     "LaplacianOfGaussian",
+    #     "StructureTensorEigenvalues",
+    #     "HessianOfGaussianEigenvalues",
+    #     "GaussianGradientMagnitude",
+    #     "DifferenceOfGaussians",
+    # ]
+    #
+    # opFeatures = workflow.featureSelectionApplet.topLevelOperator
+    # opFeatures.Scales.setValue(ScalesList)
+    # opFeatures.FeatureIds.setValue(FeatureIds)
+    #
+    # #                    sigma:   0.3    0.7    1.0    1.6    3.5    5.0   10.0
+    # selections = numpy.array(
+    #     [
+    #         [True, False, False, False, False, False, False],
+    #         [True, False, False, False, False, False, False],
+    #         [True, False, False, False, False, False, False],
+    #         [False, False, False, False, False, False, False],
+    #         [False, False, False, False, False, False, False],
+    #         [False, False, False, False, False, False, False],
+    #     ]
+    # )
+    # opFeatures.SelectionMatrix.setValue(selections)
+    #
+    # # Add some labels directly to the operator
+    # # opPixelClass = workflow.pcApplet.topLevelOperator
+    # #
+    # # opPixelClass.LabelNames.setValue(["Label 1", "Label 2"])
+    # #
+    # # slicing1 = sl[0:1, 0:10, 0:10, 0:1, 0:1]
+    # # labels1 = 1 * numpy.ones(slicing2shape(slicing1), dtype=numpy.uint8)
+    # # opPixelClass.LabelInputs[0][slicing1] = labels1
+    # #
+    # # slicing2 = sl[0:1, 0:10, 10:20, 0:1, 0:1]
+    # # labels2 = 2 * numpy.ones(slicing2shape(slicing2), dtype=numpy.uint8)
+    # # opPixelClass.LabelInputs[0][slicing2] = labels2
+    #
+    # # Train the classifier
+    # # opPixelClass.FreezePredictions.setValue(False)
+    # # _ = opPixelClass.Classifier.value
+    #
+    # # Save and close
+    # shell.projectManager.saveProject()
+    # shell.closeCurrentProject()
+    #
+    # args = "--project=" + project_file_path
+    # args += " --headless"
+    #
+    # # Batch export options
+    # args += " --output_format=tiff"
+    # args += " --output_filename_format={dataset_dir}/{nickname}_prediction.tiff"
+    # args += " --output_internal_path=volume/pred_volume"
+    # args += " --raw_data"
+    # # test that relative path works correctly: should be relative to cwd, not project file.
+    # args += " " + imagePath
+    #
+    # old_sys_argv = list(sys.argv)
+    # sys.argv = ["ilastik.py"]  # Clear the existing commandline args so it looks like we're starting fresh.
+    # sys.argv += args.split()
+    #
+    # # Start up the ilastik.py entry script as if we had launched it from the command line
+    # try:
+    #     ilastik_startup.main()
+    # finally:
+    #     sys.argv = old_sys_argv
 
     return JSONResponse({"success": True})
