2a8db72fcc70e4ef57a8c4a6c1c89c38d274eb60
james <klegendary7811@gmail.com>
2023-04-04 10:36:09 +0200

    add test process

--- ilastikApi/app/api/routers.py
+++ ilastikApi/app/api/routers.py
@@ -45,6 +45,114 @@ async def testCreateProject():
 
     return JSONResponse({"success": True})
 
+@router.get(
+    "/test_process",
+    response_description="Test for creating ilastik project",
+)
+async def testProcess():
+    projectPath = os.path.join(STATIC_PATH, 'test_ilastik_projects')
+    if not os.path.exists(projectPath):
+        os.makedirs(projectPath)
+
+    project_file_path = os.path.join(projectPath, 'TestProcess.ilp')
+    shell = HeadlessShell()
+
+    newProjectFile = ProjectManager.createBlankProjectFile(project_file_path, PixelClassificationWorkflow, [])
+    newProjectFile.close()
+
+    shell.openProjectFile(project_file_path)
+    workflow = shell.workflow
+
+    # Add a file
+    from ilastik.applets.dataSelection.opDataSelection import FilesystemDatasetInfo
+
+    info = FilesystemDatasetInfo(filePath=dataset_path)
+    opDataSelection = workflow.dataSelectionApplet.topLevelOperator
+    opDataSelection.DatasetGroup.resize(1)
+    opDataSelection.DatasetGroup[0][0].setValue(info)
+
+    # Set some features
+    ScalesList = [0.3, 0.7, 1, 1.6, 3.5, 5.0, 10.0]
+    FeatureIds = [
+        "GaussianSmoothing",
+        "LaplacianOfGaussian",
+        "StructureTensorEigenvalues",
+        "HessianOfGaussianEigenvalues",
+        "GaussianGradientMagnitude",
+        "DifferenceOfGaussians",
+    ]
+
+    opFeatures = workflow.featureSelectionApplet.topLevelOperator
+    opFeatures.Scales.setValue(ScalesList)
+    opFeatures.FeatureIds.setValue(FeatureIds)
+
+    #                    sigma:   0.3    0.7    1.0    1.6    3.5    5.0   10.0
+    selections = numpy.array(
+        [
+            [True, False, False, False, False, False, False],
+            [True, False, False, False, False, False, False],
+            [True, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False],
+        ]
+    )
+    opFeatures.SelectionMatrix.setValue(selections)
+
+    # Add some labels directly to the operator
+    opPixelClass = workflow.pcApplet.topLevelOperator
+
+    opPixelClass.LabelNames.setValue(["Label 1", "Label 2"])
+
+    slicing1 = sl[0:1, 0:10, 0:10, 0:1, 0:1]
+    labels1 = 1 * numpy.ones(slicing2shape(slicing1), dtype=numpy.uint8)
+    opPixelClass.LabelInputs[0][slicing1] = labels1
+
+    slicing2 = sl[0:1, 0:10, 10:20, 0:1, 0:1]
+    labels2 = 2 * numpy.ones(slicing2shape(slicing2), dtype=numpy.uint8)
+    opPixelClass.LabelInputs[0][slicing2] = labels2
+
+    # Train the classifier
+    opPixelClass.FreezePredictions.setValue(False)
+    _ = opPixelClass.Classifier.value
+
+    # Save and close
+    shell.projectManager.saveProject()
+    shell.closeCurrentProject()
+    del shell
+
+    sampleData = "/app/shared_static/at3.ome.tiff"
+    sampleMask = "/app/shared_static/at3_mask.jpg"
+    # NOTE: In this test, cmd-line args to tests will also end up getting "parsed" by ilastik.
+    #       That shouldn't be an issue, since the pixel classification workflow ignores unrecognized options.
+    #       See if __name__ == __main__ section, below.
+    args = "--project=" + project_file_path
+    args += " --headless"
+
+    # args += " --sys_tmp_dir=/tmp"
+
+    # Batch export options
+    args += " --output_format=tiff"
+    args += " --output_filename_format={dataset_dir}/{nickname}_prediction.tiff"
+    args += " --output_internal_path=volume/pred_volume"
+    args += " --raw_data"
+    # test that relative path works correctly: should be relative to cwd, not project file.
+    args += " " + os.path.normpath(os.path.relpath(sampleData, os.getcwd()))
+    args += " --prediction_mask"
+    args += " " + sampleMask
+
+    old_sys_argv = list(sys.argv)
+    sys.argv = ["ilastik.py"]  # Clear the existing commandline args so it looks like we're starting fresh.
+    sys.argv += args.split()
+
+    # Start up the ilastik.py entry script as if we had launched it from the command line
+    try:
+        ilastik_startup.main()
+    finally:
+        sys.argv = old_sys_argv
+
+    return JSONResponse({"success": True})
+
 @router.post(
     "/process_image",
     response_description="Process image",
