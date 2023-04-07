3f17de016ba8ac684a25e0ff9608b34a97de0144
james <klegendary7811@gmail.com>
2023-04-07 09:32:05 +0200

    change export source

--- ilastikApi/app/api/routers.py
+++ ilastikApi/app/api/routers.py
@@ -318,7 +318,7 @@ async def processImage(request: Request, files: List[UploadFile] = File(...)):
     args += " --headless"
 
     # Batch export options
-    args += " --export_source=probabilities"
+    args += " --export_source=uncertainty"
     args += " --output_format=tiff"
     args += " --output_filename_format={dataset_dir}/{nickname}_prediction.tiff"
     args += " --output_internal_path=volume/pred_volume"
