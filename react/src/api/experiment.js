e24736b87a23933182740b0ebdcbcc627ba8df79
Dimco Pajkic <120398503+devsirius44@users.noreply.github.com>
2023-04-09 04:36:13 +0200

    Small Fix (resolve conversation)

--- react/src/api/experiment.js
+++ react/src/api/experiment.js
@@ -260,8 +260,7 @@ export const MLPreprocessImage = async (original_image_url) => {
 
   let response = await api.post('image/before_process', formData, {
     headers: {
-      // 'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Origin': 'http://localhost:3000',
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': 'true',
       'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
